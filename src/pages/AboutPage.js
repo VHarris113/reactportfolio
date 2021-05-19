73de8ea80b6db9589e53cfca1ac7753b55ccba33
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-19 19:39:33 -0400

    adding the outline for the about section

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,3 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Content from "../components"
+import Content from "../components";
+
+function AboutPage(props) {
+    return (
+        <div>
+            <Hero title={props.children} />
+            <Content>
+
+            </Content>
+        </div>
+    );
+}
+
+export default AboutPage;
