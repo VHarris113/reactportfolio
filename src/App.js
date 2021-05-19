48ea4afb1c343dc99b0827875d993d9eb9830510
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-19 19:27:20 -0400

    finished footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
 class App extends React.Component {
 
   constructor(props) {
@@ -34,17 +39,25 @@ class App extends React.Component {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
-          <Navbar className="border-bottom">
-            <Navbar.Brand>Veronica Harris</Navbar.Brand>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Logo Goes Here</Navbar.Brand>
+
             <Navbar.Toggle aria-controls="navbar-toggle"/>
             <Navbar.Collapse id="navbar-toggle">
-              <Nav className="m1-auto">
+              <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.about.title} />} />
+
+          <Footer />
         </Container>
       </Router>
     )
