59eaa96a7bd751be09fb6f47a0cec41a009be3d3
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-19 19:05:15 -0400

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Container from "react-bootstrap/Container"
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 class App extends React.Component {
 
@@ -36,6 +37,13 @@ class App extends React.Component {
           <Navbar className="border-bottom">
             <Navbar.Brand>Veronica Harris</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="m1-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </Container>
       </Router>
