7f550716c857dcc2d57a6dae5586a4c0acdc847c
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-20 14:26:08 -0400

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar";
