7934134edd7f1512ac489bc0fa010eb1d1b61050
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-19 22:04:21 -0400

    working on contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,38 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function ContactPage(props) {
-    return(
-        <p>Hello from Contact</p>
-    )
+import Hero from "../components/Hero";
+import Content from "../components/Content";
+import Axios from "axios";
+
+class ContactPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            message: "",
+            disabled: "",
+            emailSent: null,
+        }
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = event.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+
 }
 
 export default ContactPage;
