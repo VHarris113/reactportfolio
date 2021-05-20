ca131f0d2d11b83ca2868cd8911f349540127c99
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-19 21:05:36 -0400

    working on carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+import Card from "../components/Card";
+
+import poketradeio from "../assets/img/poketradeio.png";
+import ecommerce2 from "../assets/gif/ecommerce2.gif";
+import dunmain from "../assets/img/Dunmain.jpg";
+import fitnesstracker from "../assets/gif/fitnesstracker.gif";
+import howtoreadme from "../assets/gif/howtoreadme.gif";
+import notetaker from "../assets/gif/notetaker.gif";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+
 class Carousel extends React.Component {
     constructor(props){
         super(props);
@@ -9,15 +21,87 @@ class Carousel extends React.Component {
                     id: 0,
                     title: "PokeTrade",
                     subTitle: "The Card Trading Site",
-                    imgSrc: "https://github.com/VHarris113/react-portfolio2/blob/9d809bc98ae9fb6b734ebdffaa13842520ed5e8f/src/assets/poketradeio.png"
+                    imgSrc: "poketradeio",
+                    link: "https://vharris113.github.io/poketrade/",
+                    repo: "https://github.com/VHarris113/poketrade",
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: "Dungeon Maintenance",
+                    subTitle: "A Bag of Holding for ALL Things D&D",
+                    imgSrc: "dunmain",
+                    link: "https://tranquil-castle-20966.herokuapp.com/",
+                    repo: "https://github.com/VHarris113/dungeon-maintenance",
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: "E-Commerce Backend",
+                    subTitle: "Keeping track of sales and inventory",
+                    imgSrc: "ecommerce2",
+                    repo: "https://github.com/VHarris113/e-commerce-backend",
+                    selected: false
+                },
+                {
+                    id: 3,
+                    title: "Fitness Tracker",
+                    subTitle: "Tracking Personal Fitness",
+                    imgSrc: "fitnesstracker",
+                    link: "https://vast-meadow-49586.herokuapp.com/",
+                    repo: "https://github.com/VHarris113/fitness-tracker",
+                    selected: false
+                },
+                {
+                    id: 4,
+                    title: "ReadMe Generator",
+                    subTitle: "Helping Keep Coding Presentable",
+                    imgSrc: "howtoreadme",
+                    repo: "https://github.com/VHarris113/readme-generator",
+                    selected: false
+                },
+                {
+                    id: 5,
+                    title: "Note Taker",
+                    subTitle: "Keeping Track of Notes",
+                    imgSrc: "notetaker",
+                    link: "https://notetaker-ex.herokuapp.com/",
+                    repo: "https://github.com/VHarris113/note-taker",
+                    selected: false
                 }
             ]
         }
     }
 
+    handleCardClick = (id, card) => {
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
     render() {
         return (
-            <p>Hello from the Carousel</p>
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
         )
     }
 }
