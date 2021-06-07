561c7911ba0a6c45177273fb442ecf43a769ef00
Veronica Harris <veronica.n.harris92@gmail.com>
2021-06-07 15:22:46 -0400

    editing homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,30 @@
-import React, { Compontent } from "react";
+import React from "react";
 import Typist from "react-typist";
 
-import Hero from "../components/Hero";
-// import Carousel from "../components/Carousel"
+// import Hero from "../components/Hero";
 
 function HomePage(props) {
     return (
-        <div>
-        <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
+        <div style={{
+            backgroundImage: `url("https://drive.google.com/file/d/1iIkT6rp8vjUbC3B8RRPsYp9csbZOmmxU/view?usp=sharing")`
+        }}>
+            <Typist>
+                    <span>Hello!</span>
+                <Typist.Backspace count={6} delay={500} />
+                    <span>My name is Veronica Harris</span>
+                    <br />
+                    <span>I'm a Back End Developer</span>
+                    <Typist.Backspace count={18} delay={200} />
+                    <span>Front End Developer</span>
+                    <Typist.Backspace count={19} delay={200} />
+                    <span>Data Analyst</span>
+                    <Typist.Backspace count={12} delay={200} />
+                    <span>Database Specialist</span>
+                    <Typist.Backspace count={19} delay={200} />
+                    <span>Full-Stack Web Developer</span>
+            </Typist>
+
+        {/* <Hero title={props.title} subTitle={props.subTitle} text={props.text}/> */}
         </div>
     )
 }
